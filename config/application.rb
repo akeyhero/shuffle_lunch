e88f53ee26c05e471151b332e8bd6ccd85eef8a2
akihiro <akihiro@kats.la>
2018-04-23 23:25:15 +0900

    タイムゾーンを東京に設定する

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module ShuffleLunch
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    config.time_zone = 'Tokyo'
+
     config.sass.preferred_syntax = :sass
 
     config.action_controller.include_all_helpers = false
