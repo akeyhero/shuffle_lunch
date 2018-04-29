0e67cb0f248451ac3b2fb47574cb006498d7292f
akihiro <akihiro@kats.la>
2018-04-29 11:46:03 +0900

    Event モデルをテスト

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { is_expected.to validate_length_of(:title).is_at_most 255 }
+    it { is_expected.to validate_length_of(:description).is_at_most 65535 }
+    it { is_expected.to validate_numericality_of(:min_member).only_integer }
+    it { is_expected.to validate_numericality_of(:min_member).is_greater_than 0 }
+    it { is_expected.to validate_numericality_of(:max_member).only_integer }
+    it { is_expected.to validate_numericality_of(:max_member).is_greater_than 0 }
+    xit { is_expected.to validate_numericality_of(:max_member).is_greater_than_or_equal_to :min_member }
+    it { is_expected.to validate_numericality_of(:max_group_member).only_integer }
+    it { is_expected.to validate_numericality_of(:max_group_member).is_greater_than 0 }
+    xit { is_expected.to validate_numericality_of(:max_group_member).is_greater_than_or_equal_to :max_member }
+  end
 end
