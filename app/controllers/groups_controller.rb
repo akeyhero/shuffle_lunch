81c1e4c224fe5858af929fc69a5c66c97f746d55
akihiro <akihiro@kats.la>
2018-10-14 17:24:27 +0900

    Group ページにチャットを追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,6 +15,6 @@ class GroupsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_group
-      @group = Group.find(params[:id])
+      @group = Group.includes(:users, messages: :user).find(params[:id])
     end
 end
